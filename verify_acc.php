3af94406df281196c2bb086a44fc9ca7d1854bb0
Kristen McGregor <50993086+Kristen-Kiki@users.noreply.github.com>
2020-02-20 15:10:26 +0200

    Add files via upload

---
+++ verify_acc.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+
+<!-- VERIFY_FORM -->
+    <h3>Verify Your Account!</h3>
+    <hr width="75%">
+
+    <div class="loginbooth2">
+        <form action="./includes/verify_acc.inc.php" method="post" align="center">
+            <input type="email" name="email" Placeholder="Insert your email address here to verify your account!" required>
+            <button type="submit" class="button button1" name="verify"> YES, ITS ME! </button>
+        </form>
+    </div>
+</body>
+</html>
