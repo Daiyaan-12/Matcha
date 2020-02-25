7f8bfa0191be7a7d9d2f97fdb588e8f9ae14b2ff
Kristen-Kiki <kmcgrego@student.wethinkcode.co.za>
2020-02-26 01:21:44 +0200

    updatebio.inc - fixed for gender bio age and pref -- still to fix interests

--- includes/updatebio.inc.php
+++ includes/updatebio.inc.php
@@ -1,129 +1,50 @@
 <?php
-session_start();
-if (isset($_POST['edit-info'])) {
     require '../config/database.php';
-    $age = ($_POST['age']);
-	$gender = ($_POST['gender']);
-	$preference = ($_POST['preference']);
-    $biography = htmlspecialchars($_POST['bio']);
-    $preference = ($_POST['sex-pref']);
-    $love = ($_POST['love']);
-    $fun = ($_POST['fun']);
-    $fitness = ($_POST['fitness']);
-    $nature = ($_POST['nature']);
-    $tech = ($_POST['tech']);
-    $meme = ($_POST['meme']);
-    $science = ($_POST['science']);
-    $animals = ($_POST['animals']);
-    $foodie = ($_POST['foodie']);
-    $user_id = $_SESSION['UserID'];
-    if (empty($gender) || empty($preference) || empty($biography) || empty($age)) {
-        header("Location: ../update_info.php?error=emptyfields");
-        exit();
-    } else if (($_POST["age"] < 18 || $_POST["age"] > 60)) {
-        header("Location: ../accountupdatedetails.php?error=tooOldorYoung");
-        exit();
-    } else if (empty($love) && empty($fun) && empty($fitness) && empty($nature) && empty($tech) && empty($meme) && empty($science) && empty($animals) && empty($foodie)) {
-        header("Location: ../accountupdatedetails.php?error=tickabox");
-        exit();
-    } else {
-        try {
-            $email = htmlspecialchars($_POST['email']);
-            $sql = "SELECT * FROM `users` WHERE email = :email";
-            $stmt = $db_connect->prepare($sql);
-            $stmt->bindParam(":email", $email);
-            $stmt->execute();
-            $count = $stmt->rowCount();
-            if ($count > 0) {
-                $result = $stmt->fetch(PDO::FETCH_ASSOC);
-                $user_id = $result['user_id'];
-            }
-				echo $user_id;
-                $sql = "UPDATE `users` SET `gender` = ? WHERE `user_id` = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1, $gender);
-                $stmt->bindParam(2, $user_id);
-				$stmt->execute();
-				echo $user_id;
-                $sql = "UPDATE `users` SET `preference` = ? WHERE `user_id` = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1, $preference);
-                $stmt->bindParam(2, $user_id);
-				$stmt->execute();
-				echo $user_id;
-                $sql = "UPDATE `users` SET `biography` = ? WHERE `user_id` = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1, $biography);
-                $stmt->bindParam(2, $user_id);
-                $stmt->execute();
-                echo $user_id;
-                $sql = "UPDATE `users` SET `age` = ? WHERE `user_id` = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1, $age);
-                $stmt->bindParam(2, $user_id);
-                $stmt->execute();
-                echo $user_id;
-                $sql = "UPDATE `users` SET `love` = ? WHERE `user_id` = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1, $love);
-                $stmt->bindParam(2, $user_id);
-                $stmt->execute();
-                echo $user_id;
-                $sql = "UPDATE `users` SET `fun` = ? WHERE `user_id` = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1, $fun);
-                $stmt->bindParam(2, $user_id);
-                $stmt->execute();
-                echo $user_id;
-                $sql = "UPDATE `users` SET `fitness` = ? WHERE `user_id` = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1, $fitness);
-                $stmt->bindParam(2, $user_id);
-                $stmt->execute();
-                echo $user_id;
-                $sql = "UPDATE `users` SET `nature` = ? WHERE `user_id` = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1, $nature);
-                $stmt->bindParam(2, $user_id);
-                $stmt->execute();
-                echo $user_id;
-                $sql = "UPDATE `users` SET `tech` = ? WHERE `user_id` = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1, $tech);
-                $stmt->bindParam(2, $user_id);
-                $stmt->execute();
-                echo $user_id;
-                $sql = "UPDATE `users` SET `meme` = ? WHERE `user_id` = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1, $meme);
-                $stmt->bindParam(2, $user_id);
-                $stmt->execute();
-                echo $user_id;
-                $sql = "UPDATE `users` SET `science` = ? WHERE `user_id` = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1, $science);
-                $stmt->bindParam(2, $user_id);
-                $stmt->execute();
-                echo $user_id;
-                $sql = "UPDATE `users` SET `animals` = ? WHERE `user_id` = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1, $animals);
-                $stmt->bindParam(2, $user_id);
-                $stmt->execute();
-                echo $user_id;
-                $sql = "UPDATE `users` SET `foodie` = ? WHERE `user_id` = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1, $foodie);
-                $stmt->bindParam(2, $user_id);
-				$stmt->execute();
-                echo "<script> window.close(); </script>";
-                header("Location: ../profile_info.php?success=InfoUpdated");
-                exit();
-            } catch (PDOException $e) {
-            die("Connection failed: " . $e->getMessage());
+
+    session_start();
+    if(isset($_POST['edit_profile']))
+    {
+        $age = trim($_POST['age']);
+        $email = trim($_POST['email']);
+        $bio = trim($_POST['bio']);
+        $gender =  trim($_POST['gender']);
+        $preference = trim($_POST['pref']);
+        $love = ($_POST['love']);
+        $fun = ($_POST['fun']);
+        $fitness = ($_POST['fitness']);
+        $nature = ($_POST['nature']);
+        $tech = ($_POST['tech']);
+        $meme = ($_POST['meme']);
+        $science = ($_POST['science']);
+        $animals = ($_POST['animals']);
+        $foodie = ($_POST['foodie']);
+
+        $sql = $db_connect->prepare("UPDATE `Users` SET age='$age', biography='$bio' WHERE email='$email'");
+        $sql->execute();
+        var_dump(); 
+        echo 'Congratulations!! Your New Identity has been Created!';
+        header('refresh:3; url="../profile/profile.php"');
+        
+        echo $row['gender'];
+        if($row['gender'] = "female" || $row['gender'] = "male" || $row['gender'] = "other")
+        {
+            $sql = $db_connect->prepare("UPDATE `Users` SET gender= '$gender' WHERE email='$email'");
+            $sql->execute();
+        }
+    
+        echo $row['pref'];
+        if($row['pref'] = "female" || $row['pref'] = "male" || $row['pref'] = "both")
+        {
+            $sql = $db_connect->prepare("UPDATE `Users` SET preference= '$preference' WHERE email='$email'");
+            $sql->execute();
+        }
+
+        echo $row['interest'];
+        if($row['pref'] = "female" || $row['pref'] = "male" || $row['pref'] = "both")
+        {
+            $sql = $db_connect->prepare("UPDATE `Users` SET preference= '$preference' WHERE email='$email'");
+            $sql->execute();
         }
     }
-} else if (isset($_POST['profile-update'])){
-    header("Location: ../edit_details.php");
-    exit();
-}
+
+?> 
--- update_info.php
+++ update_info.php
@@ -4,53 +4,112 @@
 <html>
 <head>
 <title>Profile Update</title>
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+   
+
+   <!-- style -->
+   <link rel="stylesheet" href="./css/styles.css">
+   
 </head>
 <body>
+
+
 <h1>Enter Your profile Details</h1>
-    <form action="/matcha/includes/updatebio.inc.php" method="POST">
-    <input type="text" name="age" placeholder="Age">
+
+<hr width="75%">
+
+<div class="form">
+    <form action="./includes/updatebio.inc.php" method="POST" align="center">
+
+    <label for="email"> Email Address: </label> <br/>
+        <input type="email" name="email" placeholder="Email Address" required>
+        <br>
+    <input type="text" name="age" min="18" max="40" placeholder="Age" required>
     <p>select your gender</p>
-    <select name="gender">
-        <option value="m">Male</option>
-        <option value="f">Female</option>
-        <option value="o">Other</option>
+    <input type="radio" name="gender" value="male" /> male <br />
+    <input type="radio" name="gender" value="female" /> female <br />
+    <input type="radio" name="gender" value="other" /> other <br />
+     
     </select>
     <p>What is your sexual preferance</p>
+    <!-- <input type="radio" name="pref" value="male" /> male <br />
+    <input type="radio" name="pref" value="female" /> female <br />
+    <input type="radio" name="pref" value="other" /> other <br /> -->
+
     <select name="sex-pref">
-        <option value="het">Male</option>
-        <option value="homo">Female</option>
-        <option value="bi">Both</option>
+        <option name="pref" value="male">Male</option>
+        <option name="pref" value="female">Female</option>
+        <option name="pref" value="both">Both</option>
     </select>
-    <textarea placeholder="Let everyone know a litte bit about YOU." name="bio"></textarea>
+
+    <textarea maxlength="300" placeholder="Let everyone know a litte bit about YOU." name="bio"></textarea>
     <br>
     <p>choose 3 interests</p>
     <br>
-    <input type="checkbox" id="love" name="love">
-<label for="love">#Love</label><br>
-<input type="checkbox" id="fun" name="fun">
-<label for="fun">#fun</label><br>
-<input type="checkbox" id="fitness" name="fitness">
-<label for="fitness">#Fitness</label><br>
-<input type="checkbox" id="nature" name="nature">
-<label for="nature">#Nature</label><br>
-<input type="checkbox" id="tech" name="tech">
-<label for="tech">#Technology</label><br>
-<input type="checkbox" id="meme" name="meme">
-<label for="meme">#meme</label><br>
-<input type="checkbox" id="science" name="science">
-<label for="science">#science</label><br>
-<input type="checkbox" id="animals" name="animals">
-<label for="animals">#Animals</label><br>
-<input type="checkbox" id="foodie" name="foodie">
-<label for="foodie">#foodie</label><br>
+    <input list="interest" name="interest1">
+  <datalist id="interest1">
+    <option value="#Love">
+    <option value="#Fun">
+    <option value="#Fitness">
+    <option value="#Nature">
+    <option value="#Technology">
+    <option value="#Meme culture">
+    <option value="#Science">
+    <option value="#Animals">
+    <option value="#Food Junkie">
+  </datalist>
 
-    <button type="submit" name="edit-info">Submit changes</button>
+  <input list="interest" name="interest2">
+  <datalist id="interest2">
+    <option value="#Love">
+    <option value="#Fun">
+    <option value="#Fitness">
+    <option value="#Nature">
+    <option value="#Technology">
+    <option value="#Meme culture">
+    <option value="#Science">
+    <option value="#Animals">
+    <option value="#Food Junkie">
+  </datalist>
+
+  <input list="interest" name="interest3">
+  <datalist id="interest3">
+    <option value="#Love">
+    <option value="#Fun">
+    <option value="#Fitness">
+    <option value="#Nature">
+    <option value="#Technology">
+    <option value="#Meme culture">
+    <option value="#Science">
+    <option value="#Animals">
+    <option value="#Food Junkie">
+  </datalist>
+  
+    <!-- <input type="checkbox" id="love" name="love">
+<label for="love">#Love</label><br>
+<input type="checkbox" id="fun" name="fun" >
+<label for="fun">#fun</label><br>
+<input type="checkbox" id="fitness" name="fitness" >
+<label for="fitness">#Fitness</label><br>
+<input type="checkbox" id="nature" name="nature" >
+<label for="nature">#Nature</label><br>
+<input type="checkbox" id="tech" name="tech" >
+<label for="tech">#Technology</label><br>
+<input type="checkbox" id="meme" name="meme" >
+<label for="meme">#meme</label><br>
+<input type="checkbox" id="science" name="science" >
+<label for="science">#science</label><br>
+<input type="checkbox" id="animals" name="animals" >
+<label for="animals">#Animals</label><br>
+<input type="checkbox" id="foodie" name="foodie" >
+<label for="foodie">#foodie</label><br> -->
+
+    <button type="submit" name="edit_profile">Submit changes</button>
     </form>
-    <br>
-    <form action="includes/login.inc.php" method="post">
-    <br>
-</form>
-<form action="includes/login.inc.php" method="post">
+
+
+
+<!-- <form action="includes/login.inc.php" method="post">
 <button type="submit">logout</button>
 </form>
-<a href="edit_details.php"><button>Edit account details</button></a>
+<a href="edit_details.php"><button>Edit account details</button></a> -->
