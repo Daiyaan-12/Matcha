56d5f3df56687bc7bad907bf50a307cb28164220
Daiyaan-12 <55186814+Daiyaan-12@users.noreply.github.com>
2020-02-27 20:34:15 +0200

    Add files via upload

--- profile.php
+++ profile.php
@@ -34,8 +34,23 @@ $food = $_SESSION['foodie'];
         <h2>My interests include:<br>
         <?php
         if($fun == 1)
-        echo "-Fun\n";
-        echo "hello"; 
+        echo "Fun<br>";
+        if($love == 1)
+        echo "Love<br>";
+        if($fit == 1)
+        echo "Fitness<br>";
+        if($nature == 1)
+        echo "Nature<br>";
+        if($tech == 1)
+        echo "Technology<br>";
+        if($meme == 1)
+        echo "Meme Culture<br>";
+        if($science == 1)
+        echo "Science<br>";
+        if($animals == 1)
+        echo "Animals<br>";
+        if($food == 1)
+        echo "Foodie<br>";
         ?>
         </h2>
     <a href="./update_info.php"><button>Bio Page</button></a>
