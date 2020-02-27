51fd686f5eb5f377d7a57ab623bab58db73a65df
Daiyaan-12 <55186814+Daiyaan-12@users.noreply.github.com>
2020-02-27 20:27:07 +0200

    Add files via upload

--- profile.php
+++ profile.php
@@ -1,40 +1,44 @@
 <?php
 require './config/database.php';
+//require './includes/login.inc.php';
 session_start();
-$firstName = ($_SESSION['fName']);
-$lastName = ($_SESSION['lName']);
+$firstName = ($_SESSION['firstName']);
+$lastName = ($_SESSION['lastName']);
 $bio = ($_SESSION['bio']);
 $age = ($_SESSION['age']);
+$gender = ($_SESSION['gender']);
+$pref = ($_SESSION['preference']);
+$love = $_SESSION['love'];
+$fun = $_SESSION['fun'];
+$fit = $_SESSION['fitness'];
+$nature = $_SESSION['nature'];
+$tech = $_SESSION['tech'];
+$meme = $_SESSION['meme'];
+$science = $_SESSION['science'];
+$animals = $_SESSION['animals'];
+$food = $_SESSION['foodie'];
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>My Profile</title>
-   <!-- NAV_LINKS -->
-   <link rel="stylesheet" href="./css/nav.css">
-
-
-</head>
-<body>
-
-<!-- NAV_SYSTEN -->
-<section> 
-    <nav>
-        <span id="opennav" class="open" onclick="openNav()"> &#9776; </span> <!-- burger menu tab -->
-        
-        <div id="`menu" class="nav"> 
-            <a href="#" class="closebtn" onclick="closeNav()"> &times; </a> 
-            <br>
-            <a href="./home.php"> <b> Home </b> </a> 
-            <a href="./index.php"> <b> Index </b> </a> 
-            <a href="./register.php"> <b> Sign up </b> </a>
-            <a href="./login.php"> <b> Login </b> </a>
-            <a href="./change_details.php"> <b> Bio </b> </a> 
-        </div>
-    </nav>
-</section>
-        <h1>profile of <?php echo $firstName?> <?php echo $lastName?></h1>
-
+    </head>
+    <body>
+        <h1>profile of <?php echo $firstName?> <?php echo $lastName ?></h1>
+        <h2>My age is :<?php echo $age?></h2>
+        <h2>I am a  :<?php echo $gender?></h2>
+        <h2>My  Preference is: <?php echo $pref?></h2>
+        <h2>A little bit about myself ;p<br><?php echo $bio?></h2>
+        <br>
+        <br>
+        <h2>My interests include:<br>
+        <?php
+        if($fun == 1)
+        echo "-Fun\n";
+        echo "hello"; 
+        ?>
+        </h2>
+    <a href="./update_info.php"><button>Bio Page</button></a>
         <form action="./includes/login.inc.php" method="POST">
         <button type="submit">Logout</button>
         </form>
