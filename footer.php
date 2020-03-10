e4acd7a6ab070ef0c302359c227f376d5bf6db0f
KyleZA <49956720+KyleZA@users.noreply.github.com>
2020-03-10 05:38:44 +0200

    simpler footer

--- footer.php
+++ footer.php
@@ -1,20 +1,9 @@
 <!DOCTYPE HTML>
 
 <HTML>
-<HEAD>
-<meta charset="UTF-8">
-    <TITLE>FOOTER</TITLE>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- style -->
-    <link rel="stylesheet" href="./css/nav.css">
-
-</HEAD>
-<BODY>
 <footer>
-  <div class="footer" style="text-align:center; width:100%">
-  Matcha || Sparklepony <hr> @dbadat | @kfrancis | @kmcgrego | @ntruter <br> <a href="./delete_acc.php"><div><button class="button button1">DELETE MY ACCOUNT</button></div></a>
-
+<div style="float: bottom; text-align: center; border-color: grey; border-width: 10px; border-style: solid; background-color: silver;">
+  <a>Matcha || Sparklepony</a> <hr> <a>@dbadat | @kfrancis | @kmcgrego | @ntruter</a> <hr> <a href="./delete_acc.php"><button>DELETE MY ACCOUNT</button></a>
+</div>
 </footer>
-</BODY>
 </HTML>
