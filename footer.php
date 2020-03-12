1483cc6a2623ff83315aa1c637ca503f74fd70f3
KyleZA <49956720+KyleZA@users.noreply.github.com>
2020-03-12 15:23:58 +0200

    changes made and fixed displaying info and picture

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 <HTML>
 <footer>
-<div style="float: bottom; text-align: center; border-color: grey; border-width: 10px; border-style: solid; background-color: silver;">
+<div style="clear: both; width: 100%; bottom: 0; position: relative; float: bottom; text-align: center; border-color: grey; border-width: 10px; border-style: solid; background-color: silver;">
   <a>Matcha || Sparklepony</a> <hr> <a>@dbadat | @kfrancis | @kmcgrego | @ntruter</a> <hr> <a href="./delete_acc.php"><button>DELETE MY ACCOUNT</button></a>
 </div>
 </footer>
